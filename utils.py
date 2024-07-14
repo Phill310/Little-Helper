bbbe38b5ddcb253a8ed4dc1f2d99ee838af95a52
Phill310 <mcphill310@gmail.com>
2024-07-13 17:38:35 -0700

    Make delete button a x

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ class DeleteButton(discord.ui.View):
         super().__init__(timeout=None)
         self.user_id = user_id
 
-    @discord.ui.button(label="Delete", style=discord.ButtonStyle.red)
+    @discord.ui.button(label="", style=discord.ButtonStyle.grey, emoji="❌")
     async def delete_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         if interaction.user.id == self.user_id:
             await interaction.message.delete()
