5d0b80a2a9329a6cc02994281c18b875ef679a52
Phill310 <mcphill310@gmail.com>
2024-07-13 23:42:27 -0700

    Make an embed explaining how to ask for help

---
+++ commands/getting-help.py
@@ -0,0 +1,34 @@
+from discord import app_commands
+from discord.ext import commands
+import discord
+from utils import DeleteButton
+
+
+class GettingHelpCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+        embed = discord.Embed(title="How to get help", color=bot.embed_color)
+        embed.set_footer(
+            text=bot.embed_footer,
+            icon_url=bot.embed_footer_url
+        )
+        embed.add_field(
+            name="What to send",
+            value="Once you are ready to ask your question make sure you also send some helpful information such "
+                  "as:\n- your code\n- a picture of any errors\n- a picture of /sk info\nAdditionally, "
+                  "it is important that you actually describe your issue rather than just stating that your code does "
+                  "not work or just telling us to fix it",
+            inline=False
+        )
+        embed.set_image(url="https://cdn.discordapp.com/attachments/138027100593455104/803204210291245075/image0.png")
+        self.embed = embed
+
+    @app_commands.command(name="getting-help", description="What to do when you want to ask for help")
+    @app_commands.allowed_installs(guilds=True, users=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    async def getting_help(self, interaction: discord.Interaction) -> None:
+        await interaction.response.send_message(embed=self.embed, view=DeleteButton(interaction.user.id))
+
+
+async def setup(bot):
+    await bot.add_cog(GettingHelpCog(bot=bot))
