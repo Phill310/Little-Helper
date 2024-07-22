bd242a8cede8caf90327d67e76d7027a012f1601
EquipableMC <66171067+EquipableMC@users.noreply.github.com>
2024-07-22 09:58:12 -0400

    Rename Sovdee.py to Sovde.py & add /sovde list-basics (#1)
    * Update sovdee.py
    
    * changes sovdee.py to sovde.py
    
    * Update sovde.py

--- commands/sovde.py
+++ commands/sovde.py
@@ -5,47 +5,52 @@ import discord
 from utils import DeleteButton
 
 
-class SovdeeCog(commands.Cog):
+class SovdeCog(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
-        bot.tree.add_command(Sovdee())
+        bot.tree.add_command(Sovde())
 
 
 @app_commands.allowed_installs(guilds=True, users=True)
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-class Sovdee(app_commands.Group):
+class Sovde(app_commands.Group):
     @app_commands.command(name="global-local", description="Global vs Local variables tutorial")
     async def gvars(self, interaction: discord.Interaction) -> None:
-        await interaction.response.send_message(content="[Sovdee's Global vs Local Variable Tutorial](https://sovdee.gitbook.io/skript-tutorials/core-concepts/variables/global-and-local)", view=DeleteButton(interaction.user.id))
+        await interaction.response.send_message(content="[Sovde's Global vs Local Variable Tutorial](https://sovdee.gitbook.io/skript-tutorials/core-concepts/variables/global-and-local)", view=DeleteButton(interaction.user.id))
 
     @app_commands.command(name="variables", description="Variable tutorials")
     async def vars(self, interaction: discord.Interaction) -> None:
-        await interaction.response.send_message(content="[Sovdee's Variable Tutorial](https://sovdee.gitbook.io/skript-tutorials/core-concepts/variables)", view=DeleteButton(interaction.user.id))
+        await interaction.response.send_message(content="[Sovde's Variable Tutorial](https://sovdee.gitbook.io/skript-tutorials/core-concepts/variables)", view=DeleteButton(interaction.user.id))
 
-    @app_commands.command(description="Home page of Sovdee's tutorials")
+    @app_commands.command(description="Home page of Sovde's skript tutorials")
     async def home(self, interaction: discord.Interaction) -> None:
         await interaction.response.send_message(content="https://sovdee.gitbook.io/skript-tutorials/", view=DeleteButton(interaction.user.id))
 
     @app_commands.command(name="indentation", description="Indentation and Program Flow")
     async def indent(self, interaction: discord.Interaction) -> None:
-        await interaction.response.send_message(content="[Sovdee's Indentation Tutorial]("
+        await interaction.response.send_message(content="[Sovde's Indentation Tutorial]("
                                                         "https://sovdee.gitbook.io/skript-tutorials/core-concepts/indentation)", view=DeleteButton(interaction.user.id))
 
     @app_commands.command(name="commands", description="Custom Commands")
     async def commands(self, interaction: discord.Interaction) -> None:
-        await interaction.response.send_message(content="[Sovdee's Custom Command Tutorial]("
+        await interaction.response.send_message(content="[Sovde's Custom Command Tutorial]("
                                                         "https://sovdee.gitbook.io/skript-tutorials/core-concepts/commands)", view=DeleteButton(interaction.user.id))
 
     @app_commands.command(name="vectors", description="Vector tutorials")
     async def vectors(self, interaction: discord.Interaction) -> None:
-        await interaction.response.send_message(content="[Sovdee's Vector Tutorial]("
+        await interaction.response.send_message(content="[Sovde's Vector Tutorial]("
                                                         "https://sovdee.gitbook.io/skript-tutorials/core-concepts/vectors)", view=DeleteButton(interaction.user.id))
 
     @app_commands.command(name="functions", description="Functions tutorial")
     async def functions(self, interaction: discord.Interaction) -> None:
-        await interaction.response.send_message(content="[Sovdee's Function Tutorial]("
+        await interaction.response.send_message(content="[Sovde's Function Tutorial]("
                                                         "https://sovdee.gitbook.io/skript-tutorials/core-concepts/indentation/functions)", view=DeleteButton(interaction.user.id))
+
+    @app_commands.command(name="list-basics", description="Variable List Basics Tutorial")
+    async def listvars(self, interaction: discord.Interaction) -> None:
+        await interaction.response.send_message(content="[Sovde's List Variable Tutorial]("
+                                                        "https://sovdee.gitbook.io/skript-tutorials/core-concepts/variables/list-basics)", view=DeleteButton(interaction.user.id))    
 
 
 async def setup(bot):
-    await bot.add_cog(SovdeeCog(bot=bot))
+    await bot.add_cog(SovdeCog(bot=bot))
