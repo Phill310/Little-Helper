7456a61e7a9414cb47f93c173e3685027c9f612b
Phill310 <mcphill310@gmail.com>
2024-09-18 12:40:41 -0700

    Add wiki link

--- commands/skbee.py
+++ commands/skbee.py
@@ -16,8 +16,10 @@ class SkBeeCog(commands.Cog):
         file = discord.File("SkBee-3.6.1.jar")
         await interaction.response.send_message(
             content=ping + "SkBee has been discontinued by its owner. Other addons or forks may exist or be made to "
-                           "replace it. In the meantime, the latest SkBee jar is attached below and documentation can "
-                           "be found [here](https://web.archive.org/web/20240913065946/https://skripthub.net/docs/)",
+                           "replace it. In the meantime, the latest SkBee jar is attached below. Documentation can "
+                           "be found [here](<https://web.archive.org/web/20240913065946/https://skripthub.net/docs"
+                           "/>). More indepth explanations can be found in [the wiki]"
+                           "(<https://web.archive.org/web/20240910012111/https://github.com/ShaneBeee/SkBee/wiki>).",
             file=file
         )
 
