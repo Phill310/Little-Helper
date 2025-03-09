d20df9fe3a14d76f525c7d0e27b2d531949ce305
Phill310 <mcphill310@gmail.com>
2025-03-08 18:33:05 -0800

    Add command to explain new item flag issues

---
+++ commands/hide-attributes.py
@@ -0,0 +1,27 @@
+from discord import app_commands
+from discord.ext import commands
+import discord
+import utils
+
+
+class HideAttributesCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+
+    @app_commands.command(description="Explain that the attribute itemflag doesn't work on 1.20.5+")
+    @app_commands.describe(reply_to="The user you want to send this message to")
+    @app_commands.allowed_installs(guilds=True, users=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    async def hide_attributes(self, interaction: discord.Interaction, reply_to: discord.User = None) -> None:
+        await utils.send(
+            interaction=interaction,
+            content="If you are playing on Minecraft 1.20.5+ the hide attributes itemflag will not work because "
+                    "this behavior is now handled by the attributes themselves. You can use [this method]"
+                    "(<https://github.com/ShaneBeee/SkBee/wiki/Tricks-Hide-Attribute-Modifiers>) to hide default "
+                    "attributes now",
+            ping=reply_to
+        )
+
+
+async def setup(bot):
+    await bot.add_cog(HideAttributesCog(bot=bot))
