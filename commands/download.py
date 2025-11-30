c305689f603250695c1d20057562d579c9311c63
Phill310 <mcphill310@gmail.com>
2025-11-29 17:12:26 -0800

    Update embed to follow new release model

--- commands/download.py
+++ commands/download.py
@@ -16,7 +16,7 @@ class DownloadCog(commands.Cog):
     def update_embed(self):
         embed = discord.Embed(title="", color=self.bot.embed_color, timestamp=datetime.datetime.now())
         embed.set_footer(
-            text="Embed designed by jakegblp",
+            text=self.bot.embed_footer,
             icon_url=self.bot.embed_footer_url
         )
 
@@ -41,26 +41,18 @@ class DownloadCog(commands.Cog):
                         break
                     elif int(current_version[i]) < int(highest_version_split[i]):
                         break
-        latest = "> Minecraft **1.19.4+**: [Skript " + latest_version + "](" + latest_data["html_url"] + ")"
+        latest = "[Skript " + latest_version + "](" + latest_data["html_url"] + ")"
         if latest_unstable_version != 0 and latest_unstable_version != latest_version:
             latest += "\n### Latest Beta:\n> Not recommended for production servers: [Skript " + latest_unstable_version + "](" + latest_unstable_data[
                 "html_url"] + ")"
 
-        mato_data = json.loads(requests.get("https://api.github.com/repos/Matocolotoe/Skript-1.8/releases/latest").text)
-        mato_version = mato_data["tag_name"]
-
         embed.description = f"""
-# Skript Downloads
-These are the recommended versions of Skript:
+# Download Skript
+Skript supports minecraft versions that were released up to 18 months before the release of each Skript version. 
 ### Latest Release:
 {latest}
-### Older Releases:
-> Minecraft **1.9** - **1.12.2**: [Skript 2.6.4](https://github.com/SkriptLang/Skript/releases/tag/2.6.4)
-> Minecraft **1.13** - **1.19.3**: [Skript 2.9.5](https://github.com/SkriptLang/Skript/releases/tag/2.9.5)
 
-### Unofficial Releases:
-> Minecraft **1.8.x**: [Matocolotoe fork {mato_version}]({mato_data["html_url"]})
--# These versions are not supported by SkriptLang
+If you would like to use an older version of Skript you can search through the [releases](https://github.com/SkriptLang/Skript/releases) section of the github.
 """
         self.embed = embed
         self.next_version_check = datetime.datetime.now() + datetime.timedelta(hours=4)
