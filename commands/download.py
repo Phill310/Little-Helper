bbe335743da7341a4de07a92af4f238d695f98b1
Phill310 <mcphill310@gmail.com>
2026-02-06 13:08:37 -0800

    Calculate which mc versions are supported

--- commands/download.py
+++ commands/download.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import discord
 import requests
 import json
-import datetime
+from datetime import datetime, timedelta
 import utils
 
 
@@ -12,9 +12,21 @@ class DownloadCog(commands.Cog):
         self.next_version_check = None
         self.embed = None
         self.bot = bot
+        self.update_embed()
+
+    def get_date_18_month_ago(self, date: datetime):
+        month = date.month
+        year = date.year
+        if month <= 6:
+            year -= 1
+            month += 12
+        month -= 6
+        year -= 1
+
+        return date.replace(month=month, year=year)
 
     def update_embed(self):
-        embed = discord.Embed(title="", color=self.bot.embed_color, timestamp=datetime.datetime.now())
+        embed = discord.Embed(title="", color=self.bot.embed_color, timestamp=datetime.now())
         embed.set_footer(
             text=self.bot.embed_footer,
             icon_url=self.bot.embed_footer_url
@@ -22,29 +34,38 @@ class DownloadCog(commands.Cog):
 
         latest_data = json.loads(requests.get("https://api.github.com/repos/SkriptLang/Skript/releases/latest").text)
         latest_version = latest_data["tag_name"]
+        latest_release_date = datetime.fromisoformat(latest_data["published_at"])
         latest_releases = json.loads(requests.get("https://api.github.com/repos/SkriptLang/Skript/releases").text)[:10]
-        latest_unstable_data = 0
-        latest_unstable_version = 0
-        highest_version_split = 0
+        latest_unstable_data = None
+        if "pre" in latest_releases[0]["tag_name"]:
+            if datetime.fromisoformat(latest_releases[0]["published_at"]) > latest_release_date:
+                latest_unstable_data = latest_releases[0]
+        latest_feature = None
+        if latest_version.split(".")[-1] == "0":
+            latest_feature = latest_release_date
+
         for release in latest_releases:
-            if latest_unstable_data == 0:
-                latest_unstable_data = release
-                latest_unstable_version = release["tag_name"]
-                highest_version_split = release["tag_name"].split("-")[0].split(".")
+            if not latest_feature and release["tag_name"].split(".")[-1] == "0":
+                latest_feature = datetime.fromisoformat(release["published_at"])
+                print("using", release["tag_name"], "version:", latest_feature)
+                break
+
+        minecraft_releases = [release for release in json.loads(requests.get("https://piston-meta.mojang.com/mc/game/version_manifest.json").text)["versions"] if release["type"] == "release"]
+        supported_releases = []
+        oldest_supported_date = self.get_date_18_month_ago(latest_feature)
+        for release in minecraft_releases:
+            release_date = datetime.fromisoformat(release["releaseTime"])
+            if release_date > latest_release_date:
+                continue
+            if release_date >= oldest_supported_date:
+                supported_releases.append(release["id"])
             else:
-                current_version = release["tag_name"].split("-")[0].split(".")
-                for i in range(3):
-                    if int(current_version[i]) > int(highest_version_split[i]):
-                        latest_unstable_data = release
-                        latest_unstable_version = release["tag_name"]
-                        highest_version_split = release["tag_name"].split("-")[0].split(".")
-                        break
-                    elif int(current_version[i]) < int(highest_version_split[i]):
-                        break
-        latest = "[Skript " + latest_version + "](" + latest_data["html_url"] + ")"
-        if latest_unstable_version != 0 and latest_unstable_version != latest_version:
-            latest += "\n### Latest Beta:\n> Not recommended for production servers: [Skript " + latest_unstable_version + "](" + latest_unstable_data[
-                "html_url"] + ")"
+                supported_releases.append(release["id"])
+                break
+
+        latest = f"Minecraft **{supported_releases[-1]}** - **{supported_releases[0]}**: [Skript " + latest_version + "](" + latest_data["html_url"] + ")"
+        if latest_unstable_data:
+            latest += "\n### Latest Beta:\n> Not recommended for production servers: [Skript " + latest_unstable_data["tag_name"] + "](" + latest_unstable_data["html_url"] + ")"
 
         embed.description = f"""
 # Download Skript
@@ -55,14 +76,14 @@ Skript supports minecraft versions that were released up to 18 months before the
 If you would like to use an older version of Skript you can search through the [releases](https://github.com/SkriptLang/Skript/releases) section of the github.
 """
         self.embed = embed
-        self.next_version_check = datetime.datetime.now() + datetime.timedelta(hours=4)
+        self.next_version_check = datetime.now() + timedelta(hours=4)
 
     @app_commands.command(description="Informational embed on downloading Skript")
     @app_commands.describe(reply_to="The user you want to send this message to")
     @app_commands.allowed_installs(guilds=True, users=True)
     @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
     async def download(self, interaction: discord.Interaction, reply_to: discord.User = None) -> None:
-        if self.embed is None or self.next_version_check < datetime.datetime.now():
+        if self.embed is None or self.next_version_check < datetime.now():
             self.update_embed()
         await utils.send(
             interaction=interaction,
